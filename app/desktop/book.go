a05bc7dc78e46680162aedb6b31eab86c15d702a
Javad <ja7ad@live.com>
2023-09-20 21:13:25 +0330

    update: desktop version

--- app/desktop/book.go
+++ app/desktop/book.go
@@ -21,7 +21,7 @@ import (
 //go:embed content
 var Book embed.FS
 
-const VERSION = "5.2.0"
+const VERSION = "5.2.1"
 
 func main() {
 	port := flag.Int64("port", 8080, "port for listen and serve example 8080")
